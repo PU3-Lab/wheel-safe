d8e62a1df4286d6a1361b393d5ea7588f38adef2
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2026-03-04 00:16:35 +0900

    path.py 추가

---
+++ src/lib/utils/path.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from pyprojroot import here
+
+root = here()
+
+
+def data_path():
+    path = root / 'data'
+    Path.mkdir(path, parents=True, exist_ok=True)
+    return path
+
+
+def model_path():
+    path = root / 'data' / 'models'
+    Path.mkdir(path, parents=True, exist_ok=True)
+    return path
+
+
+def raw_data_path(depth_num):
+    path = root / 'data' / 'raw' / f'depth_{depth_num}'
+    Path.mkdir(path, parents=True, exist_ok=True)
+    return path
