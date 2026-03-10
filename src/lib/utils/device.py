5d866b94ef185d0c33052b86f510b3a82feb8c06
KimKyungPyo <kyungpyo.kim0@gmail.com>
2026-03-11 08:48:51 +0900

    사용중인 gpu 로그

--- src/lib/utils/device.py
+++ src/lib/utils/device.py
@@ -4,7 +4,10 @@ import torch
 
 
 def get_device():
+    device = 'cpu'
     if sys.platform == 'win32' or sys.platform == 'linux':
-        return 'cuda' if torch.cuda.is_available() else 'cpu'
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
     else:
-        return 'mps' if torch.backends.mps else 'cpu'
+        device = 'mps' if torch.backends.mps else 'cpu'
+    print(f'Using {device}')
+    return device
